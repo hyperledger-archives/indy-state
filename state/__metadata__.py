0e88b0979a59d3b4515e6c76793938102618e5c5
andkononykhin <andkononykhin@gmail.com>
2017-04-13 14:43:57 +0000

    fix for metadata routine (#2)
    * jenkins routine
    
    * updated names
    
    * updated version info to match common approach
    
    * fix
    
    * activated pypi publishing
    
    * skips debs publishing with common test and publish api usage
    
    * fixed version construction

--- state/__metadata__.py
+++ state/__metadata__.py
@@ -2,7 +2,7 @@
 patricia tree package metadata
 """
 __version_info__ = (0, 1)
-__version__ = '{}.{}'.format(*__version_info__)
+__version__ = '.'.join(map(str, __version_info__))
 __author__ = "Evernym, Inc."
 __license__ = "Apache 2.0"
 
