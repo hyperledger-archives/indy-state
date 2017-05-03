3bfe3198629fcb26e540f0e45a062d2e825c2e49
Dmitry Surnin <dmitry.surnin@dsr-company.com>
2017-05-03 13:25:51 +0300

    Add rocksdb as a key-value backend

--- state/kv/kv_store_rocksdb.py
+++ state/kv/kv_store_rocksdb.py
@@ -1,19 +1,73 @@
 from typing import Iterable, Tuple
-from state.kv.kv_store import KeyValueStorage
 
-# TODO: WIP below
+import shutil
+from state.kv.kv_store import KeyValueStorage
+from state.util.utils import removeLockFiles
+
+try:
+    import rocksdb
+except ImportError:
+    print('Cannot import rocksdb, please install')
+
+
 class KeyValueStorageRocksdb(KeyValueStorage):
-    def set(self, key, value):
-        raise NotImplementedError
+    def __init__(self, dbPath, open=True):
+        if 'rocksdb' not in globals():
+            raise RuntimeError('Rocksdb is needed to use this class')
+        self._dbPath = dbPath
+        self._db = None
+        if open:
+            self.open()
+
+    def open(self):
+        self._db = rocksdb.DB(self._dbPath, rocksdb.Options(create_if_missing=True))
+
+    def __repr__(self):
+        return self._dbPath
+
+    def put(self, key, value):
+        if isinstance(key, str):
+            key = key.encode()
+        if isinstance(value, str):
+            value = value.encode()
+        self._db.put(key, value)
 
     def get(self, key):
-        raise NotImplementedError
+        if isinstance(key, str):
+            key = key.encode()
+        return self._db.get(key)
 
     def remove(self, key):
-        raise NotImplementedError
+        if isinstance(key, str):
+            key = key.encode()
+        self._db.delete(key)
 
     def setBatch(self, batch: Iterable[Tuple]):
-        raise NotImplementedError
+        b = rocksdb.WriteBatch()
+        for key, value in batch:
+            if isinstance(key, str):
+                key = key.encode()
+            if isinstance(value, str):
+                value = value.encode()
+            b.put(key, value)
+        self._db.write(b, sync=False)
 
     def close(self):
-        raise NotImplementedError
+        removeLockFiles(self._dbPath)
+        del self._db
+        self._db = None
+
+    def drop(self):
+        self.close()
+        shutil.rmtree(self._dbPath)
+
+    def iter(self, start=None, end=None, include_value=True):
+        if not include_value:
+            return self._db.iterkeys()
+        else:
+            return self._db.iteritems()
+
+    def has_key(self, key):
+        if isinstance(key, str):
+            key = key.encode()
+        return self._db.key_may_exist(key)[0]
